380e684a49a06ab535618eb8730bff747d1ffb47
JGWill <jgi@jgwill.com>
2021-03-14 15:22:38 -0400

    cleanup

--- dns.updater.2103141506.sh
+++ dns.updater.2103141506.sh
@@ -7,12 +7,14 @@
 ## 0 6,9,12,15,18,21,0,3 * * 1 /a/bin/dns.updater.2103141506.sh
 
 
-urlcall="https://directnic.com/dns/gateway/2ac6e31108282aaa24de43d4f45213ef76e566f563bf5649eac74c9be07dde3f/?data="
+source /a/bin/_env.sh
+
+urlcall="https://directnic.com/dns/gateway/$asdnstoken/?data="
 currentip="10.10.22.255"
 
 currentip=$(ip addr | grep enp3 | awk '/inet/ { print $4 }')
 
-echo $currentip
+echo Current IP is : $currentip
 
 urlfullcall="$urlcall$currentip"
 
