d75cef77bc28471952b65441c8ab6b90fe35d3cf
JGWill-Orko <jgi@jgwill.com>
2021-05-07 11:27:35 -0400

    chg:prints-only-file-with-searched-pattern

--- grepSearch.sh
+++ grepSearch.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 #@STCGoal Search in folder a pattern and learn where it is
-if [ "" == "" ]; then
+if [ "$1" == "" ]; then
 	echo "Usage : greps <Pattern to search in> [<Prefix pattern to search from> <Suffix ...> ]
 	* greps bash_env .bash sh
 
 	"
 	exit
 fi
+pattern=$1
 for f in $2*$3; do
-	echo "### $f"
-	cat $f | grep $1
+#	echo "################################################################"
+#	echo "###################################### $f ######################"
+	pattern=$1
+	#for f in *php ; do r=$(cat $f | grep $pattern);if [ "$r" != "" ]; then echo "----- $f" ; fi; done
+	
+	r=$(cat $f | grep $pattern)
+	if [ "$r" != "" ]; then 
+		echo "----- $f"  
+	fi
+
+	#cat $f | grep $1
 done
 
