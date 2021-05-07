3778c6e1235e12a580d027798ef138350411cde2
JGWill-Orko <jgi@jgwill.com>
2021-05-07 08:34:33 -0400

    feat(dk):remove container

---
+++ dkcRemove.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#@STCGoal 
+
+if [ -e $binroot/__fn.sh ]; then source $binroot/__fn.sh ; fi
+
+envif
+exitifnoval "Usage $0 <container name> 
+	Stop and remove a container
+        " $1
+
+# 
+docker stop $1 && docker rm $1
+
+
+
+
+
+
