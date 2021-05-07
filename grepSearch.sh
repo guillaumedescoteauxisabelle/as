0e459fb556d890e103ff76e1abacd472fb93212e
JGWill-Orko <jgi@jgwill.com>
2021-05-07 05:11:44 -0400

    doc

--- grepSearch.sh
+++ grepSearch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 #@STCGoal Search in folder a pattern and learn where it is
+if [ "" == "" ]; then
+	echo "Usage : greps <Pattern to search in> [<Prefix pattern to search from> <Suffix ...> ]
+	* greps bash_env .bash sh
+
+	"
+	exit
+fi
 for f in $2*$3; do
 	echo "### $f"
 	cat $f | grep $1
