72159abadf7fcba3dd1e63919fd51b1562eeaec0
JGWill-Orko <jgi@jgwill.com>
2021-05-08 13:24:26 -0400

    refactoÈ

--- _ast_for_port_range.sh
+++ _ast_for_port_range.sh
@@ -8,24 +8,41 @@ label=PT_to_jg_compos51_56
 labelprefix=PT__
 #Subdir
 xdir=samples-vue-des-artistes-210606
+dropboxlocalpath=$HOME/Dropbox
+
 echo "
 #@STCGoal One file is stylized with each port within range of specified port and 
 USAGE: $0 <file> <startport> <endport>
 ";
 
 f=$1
-fnb=${f%.*}
 startport=$2
 endport=$3
+
+giastcmd="gia-ast"
+giastdefaultsuffix='__stylize'
+galprefix='gal_'
+giastdefaultext='jpg'
+fpprefix='s_'
+# Copy the content in the source as the first of the sequence
+tcontentsuffix='__000'
+
+fnb=${f%.*}
 cdir=$(pwd)
 
 label=$labelprefix'__'$fnb
-dropboxlocalpath=$HOME/Dropbox
 odir=$dropboxlocalpath/$lpath
 rdir=$odir/$xdir
-fp='s_'$label
+fp=$fpprefix$label
 tdir=$rdir/$fp
 ldir=/$lpath/$xdir/$fp
+
+csmtargetdir='..'
+csmprefix='_'
+
+
+
+tcontentfile=$fnb$tcontentsuffix.$giastdefaultext
 
 echo $tdir
 mkdir -p $tdir
@@ -33,22 +50,21 @@ sleep 1
 
 for p in $(seq $startport $endport); do
 	echo $p
- 	gia-ast $f $p
-        echo mv $fnb'__stylize'*.jpg $tdir	
-        mv $fnb'__stylize'*.jpg $tdir	
+ 	$giastcmd $f $p
+        echo mv $fnb$giastdefaultsuffix*.$giastdefaultext $tdir	
+        mv $fnb$giastdefaultsuffix*.$giastdefaultext $tdir	
 
 
 done
 
-# Copy the content in the source as the first of the sequence
-tcontentfile=$fnb'__000.jpg'
+
 tctargetpath=$tdir/$tcontentfile
 cp -f $f $tctargetpath
 
 # Make the sheet
 (cd $tdir; gis-csm -d --label --verbose) &
 sleep 2
-. /a/bin/gallery_html_maker.sh $tdir 'gal_'$tdir
+. /a/bin/gallery_html_maker.sh $tdir $galprefix$tdir
 
 echo "See in $tdir"
 # make a little HTML presenter
@@ -58,7 +74,9 @@ idx=index.html
 cd $tdir
 droxul -q mkdir $ldir
 sleep 5
-droxul upload '../_'$fp*csm.* $ldir
+
+
+droxul upload $csmtargetdir/$csmprefix$fp*csm.* $ldir
 for f in *jpg; do droxul -q upload $f $ldir ; done
 
 #back
