f264825377429c4d7076ff0b7284d8057edb4d4a
Jean Guillaume <jgi@jgwill.com>
2021-05-09 19:00:20 -0400

    chg

--- chk_to_gallery.sh
+++ chk_to_gallery.sh
@@ -1,6 +1,10 @@
  #!/bin/bash
 
- chk=240k
- for f in $(du -a | grep $chk | awk '// { print $2 $3 $4 }'); do 
+ chk=$1
+ outf=$2
+for f in $(du -a | grep $chk | awk '// { print $2 $3 $4 }')
+  do 
   echo '![]('$f')' 
+  echo $"\n----\n"
 done
+
