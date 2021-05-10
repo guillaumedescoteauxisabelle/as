5686038d26aa4fe7e13433ec545b0883da5c090d
JGWill-Orko <jgi@jgwill.com>
2021-05-10 10:14:46 -0400

    add:start-stop-container

--- dkcstop.sh
+++ dkcstop.sh
@@ -10,28 +10,26 @@ envif $@
 
 DEBUG=0
 lookquiet $@
-startapp "Docker Utilities - Container remover" \
+startapp "Docker Utilities - Container Stopper" \
         "Guillaume Descoteaux-Isabelle" \
         2021 \
         " 
 Usage dkcrm <container name> 
-	Stop and remove a container" \
+	Stop a container" \
         $1 #--quiet watch 
 
 #exitifnoval "$appusage" $1
 
 
-dowork $1 "Stopping and deleting $1" 
+dowork $1 "Stopping  $1" 
 
 
 if [ "$QUIET" == "1" ]; then
-docker stop -t 1 $1  > /dev/null 2>&1 && \
-        docker rm $1   > /dev/null 2>&1 && \
+docker stop -t 1 $1  > /dev/null 2>&1 && \ 
         donework "SUCCESS" "$2" "$3" && exit 0 || \
         einq "FAILED" $2 && exit 1
 else
-docker stop -t 1 $1    && \
-        docker rm $1    && \
+docker stop -t 1 $1    && \ 
         donework "SUCCESS" "$2" "$3" && exit 0 || \
         einq "FAILED" $2 && exit 1
 fi
