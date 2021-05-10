f4aca5f8a7a0bb44910de250789577e10285366d
JGWill-Orko <jgi@jgwill.com>
2021-05-09 20:58:14 -0400

    add:Var

--- fndkGetAvailableModels.sh
+++ fndkGetAvailableModels.sh
@@ -2,9 +2,11 @@
 
 #@STCGoal A List of available port we can use along with the ability to list and know what model is deployed on it
 #@STCIssue  We do not know what we can call and we might change inferencing scenarios.
-
+cfilter="ast"
 if [ "$rwroot" != "" ]; then
-	export dkrunningcontainerports=$(cd $rwroot;./_adm__stopRemove_Containers__210502.sh --list --port| tr "\n" " " )
+	export dkrunningcontainerports=$(cd $rwroot;./_adm__stopRemove_Containers__210502.sh --list --port | grep $cfilter)
+	#| tr " " "\n"  )
+	echo $dkrunningcontainerports > $wwwroot/data/dkrunningcontainerports.txt
 else
 	#dkrunningcontainers
 	export dkrunningcontainers=SOMEISSUES
