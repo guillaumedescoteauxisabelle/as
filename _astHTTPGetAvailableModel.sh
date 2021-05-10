628b31b04865cb5c2c94a602acc0dc085d6a1bad
Jean Guillaume <jgi@jgwill.com>
2021-05-09 21:32:12 -0400

    add:proto to get available model from HTTP call

---
+++ _astHTTPGetAvailableModel.sh
@@ -0,0 +1,10 @@
+
+
+#@STCGoal Available list of model service available from network commands
+# Most likely :  gia-ast --list
+
+asthostserver=orko
+dkrunningcontainerports.txt;
+wget http://$asthostserver/data/dkrunningcontainerports.txt  > /dev/null 2>&1
+dkrunningcontainerports=$(cat dkrunningcontainerports.txt) 
+echo "$dkrunningcontainerports" | tr " " "\n"
