9675ae4baf91385cfa9a950a14c821ece25f5561
JGWill-Orko <jgi@jgwill.com>
2021-05-12 02:59:52 -0400

    refacto

--- fndkGetAvailableModels.sh
+++ fndkGetAvailableModels.sh
@@ -3,10 +3,14 @@
 #@STCGoal A List of available port we can use along with the ability to list and know what model is deployed on it
 #@STCIssue  We do not know what we can call and we might change inferencing scenarios.
 cfilter="ast"
+webdatafolder=$wwwroot/data
+mkdir -p $webdatafolder
+webdatafile=$webdatafolder/dkrunningcontainerports.txt
+
 if [ "$rwroot" != "" ]; then
 	export dkrunningcontainerports=$(cd $rwroot;./_adm__stopRemove_Containers__210502.sh --list --port | grep $cfilter)
 	#| tr " " "\n"  )
-	echo $dkrunningcontainerports > $wwwroot/data/dkrunningcontainerports.txt
+	echo $dkrunningcontainerports > $webdatafile
 else
 	#dkrunningcontainers
 	export dkrunningcontainers=SOMEISSUES
