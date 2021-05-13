40f8eaa87dfc1c3b206f5cf121b1ca9e0c0617a9
Jean Guillaume <jgi@jgwill.com>
2021-05-13 14:16:16 -0400

    tmp:gaia

--- _op_cp__loved__210513.sh
+++ _op_cp__loved__210513.sh
@@ -5,10 +5,35 @@ fi
 
 
 
-v=$(make_dirns "$1" "$2" "$3")
+# v=$(mkdirns "$1" "$2" "$3")
 
-echo '> echo '"GIAOUTDIRNS=$v"
+# echo '> echo '"GIAOUTDIRNS=$v"
 
-echo "-------------------------"
-make_dirns "$1" "$2" "$3"
-#for d in /c/Users/jeang/Dropbox/lib/results/gia-ds-fpolsonwill_v02_210424/fp_s02-v02/_loved /c/Users/jeang/Dropbox/lib/results/frank_polson_v01_210420__210421/_loved /c/Users/jeang/Dropbox/lib/results/frank_polson_v01_210420__210423_loved_pass2 /c/Users/jeang/Dropbox/lib/results/cecinestpasunecompagnie__210420__compoStylized210420/_loved /c/Users/jeang/Dropbox/lib/results/viromunchki_segmented__210419__compoStylized/_loved /c/Users/jeang/Dropbox/lib/results/viromunchkin-s01-v01_new/_loved /c/Users/jeang/Dropbox/lib/results/daliv1-2101240443/daly__loved /c/Users/jeang/Dropbox/lib/results/Sketch__2101240002__pass02/_loved ; do t=$(echo $d  | tr "/" " "| awk // { print __ } | sed -e s/results__/ma_ast_res__/); (cd $d ; tar cf - $d | (mkdir $t && cd $_ && tar xvf -); done
+#echo "-------------------------"
+#mkdirns "$1" "$2" "$3"
+
+listfilepath=$libroot/pf/_ma__ast__res__210513.ls.txt
+toprocess=$(cat $listfilepath)
+cdir=$(pwd)
+mkdirnslevel=2
+stripofcharchar=("results__" "lib__" "gia-ds-")
+#echo "$stripofcharchar"
+a="absc"
+#stripof "$1" "$@" "$stripofcharchar"
+stripof "$1" "$@" "$stripofcharchar"
+#exit 1
+for d in $toprocess ; do 
+	cd $d
+	
+	#t=$(echo $d  | tr "/" " "| awk // { print __ } | sed -e s/results__/ma_ast_res__/)
+	# dirns=$(mkdirns "$d" "$mkdirnslevel")
+	# #echo $dirns
+	# t=$(stripof "$dirns" "${stripofcharchar[@]}")
+	#t=$(mkdirnstrippedof "$d" "$mkdirnslevel" "${stripofcharchar[@]}")
+	echo "t=\$(mkdirnstrippedof \"$d\" \"$mkdirnslevel\" \"${stripofcharchar[@]}\")"
+
+	#echo "stripofcharchar=${stripofcharchar[@]}"
+	#echo "$t"
+	#(cd $d ; tar cf - $d | (mkdir $t && cd $_ && tar xvf -)
+done
+cd $cdir
