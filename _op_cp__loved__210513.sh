6af6d179ec6a85eb8d891e44bb59659f0363a38f
Jean Guillaume <jgi@jgwill.com>
2021-05-13 14:47:57 -0400

    upgrade

--- _op_cp__loved__210513.sh
+++ _op_cp__loved__210513.sh
@@ -18,22 +18,34 @@ cdir=$(pwd)
 mkdirnslevel=2
 stripofcharchar=("results__" "lib__" "gia-ds-")
 #echo "$stripofcharchar"
-a="absc"
+#a="absc"
 #stripof "$1" "$@" "$stripofcharchar"
-stripof "$1" "$@" "$stripofcharchar"
+#stripof "$1" "$@" "$stripofcharchar"
 #exit 1
 for d in $toprocess ; do 
 	cd $d
 	
 	#t=$(echo $d  | tr "/" " "| awk // { print __ } | sed -e s/results__/ma_ast_res__/)
-	# dirns=$(mkdirns "$d" "$mkdirnslevel")
+	dirns=$(mkdirns "$d" "$mkdirnslevel")
 	# #echo $dirns
-	# t=$(stripof "$dirns" "${stripofcharchar[@]}")
+	t=$(stripof "$dirns" "${stripofcharchar[@]}")
 	#t=$(mkdirnstrippedof "$d" "$mkdirnslevel" "${stripofcharchar[@]}")
-	echo "t=\$(mkdirnstrippedof \"$d\" \"$mkdirnslevel\" \"${stripofcharchar[@]}\")"
+	#echo "t=\$(mkdirnstrippedof \"$d\" \"$mkdirnslevel\" \"${stripofcharchar[@]}\")"
 
 	#echo "stripofcharchar=${stripofcharchar[@]}"
-	#echo "$t"
-	#(cd $d ; tar cf - $d | (mkdir $t && cd $_ && tar xvf -)
+	echo "$t"
+	(cd $d ; tar cf - . | (cd $cdir;mkdir $t && cd $_ && tar xvf - && mv **/* . && rm -rf  **/**/build** && rm -rf  **/build** && rm **/*.csm.*) )
+
 done
 cd $cdir
+echo 'Might want to run this:
+
+539  rm -rf  **/build**
+  540  rm -rf  **/**/build**
+  541  rm -rf  **/**/**/build**
+  542  rm -rf  **/**/**/**/build**
+  543  rm -rf  **/**/**/**/**/build**
+  544  rm -rf  **/**/**/**/**/build
+
+rm **/*.csm.*
+
