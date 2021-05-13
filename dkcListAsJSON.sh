64839675b173dea8e0be478b36e19752c922b713
JGWill-Orko <jgi@jgwill.com>
2021-05-13 14:15:51 -0400

    fix:dos2unix

--- dkcListAsJSON.sh
+++ dkcListAsJSON.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+containers=$(docker ps | awk '{if(NR>1) print $NF}')
+
+
+for container in $container ; do
+	cports=$(docker container port $container | tr ":" " "| awk '/0.0.0.0/ { print $4}')
+	echo $cports
+
+done
+
+
 docker container ps \
-	--format="{\"name\":\"{{.Names}}\",\"ports\":\"{{.Ports}}\",\"image\":\"{{.Image}}\",\"status\":\"{{.Status}}\"}" \
+	 --format="{\"name\":\"{{.Names}}\",\"ports\":\"{{.Ports}}\",\"image\":\"{{.Image}}\",\"status\":\"{{.Status}}\"}" \ 
 	--all | jq --slurp
+
+
