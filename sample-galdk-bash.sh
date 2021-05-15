fe51e48b8dd297f25ca06bba56aa4144540c5d50
JGWill-Orko <jgi@jgwill.com>
2021-05-15 09:04:50 -0400

    sample

---
+++ sample-galdk-bash.sh
@@ -0,0 +1,6 @@
+#on the host
+/a/bin/galdk-bash.sh /a/lib/results/gia-ds-DavidBouchardGagnon-v01a-210510/dbg_v01/1024x /a/lib/results/gia-ds-DavidBouchardGagnon-v01a-210510/dbg_v01/1024x__gal
+
+# on the docker container
+/a/bin/gallery_html_maker2.sh /input/ /output/ "gal dbg jg"  "gal dbg jg" "gal dbg jg"
+
