f13955b5c6d810bc6d1db9e9af430a4b728ea975
JGWill-Orko <jgi@jgwill.com>
2021-05-16 06:22:55 -0400

    feat: makecheckpointfile

--- __fn.sh
+++ __fn.sh
@@ -452,3 +452,106 @@ getmodellist()
 {
 	 for d in $(cd $modelroot;ls -d *| grep model_); do (cd $modelroot;if [ -d "$d" ];then echo $d ; fi) ; done
 }
+
+
+
+
+makecheckpointfile(){
+
+
+	#########################################################
+	# Model Web API Checkpoint Server                       #
+	# This version should receive a checkpoint number       #
+	# as input to mount along with the modelname            #
+	# By Guillaume Descoteaux-Isabelle, 2021 (UQAC/NAD)     #
+	#########################################################
+
+	# 
+
+
+	source $rwroot/_env.sh
+	modelname=$1
+	checkpointno=$2
+	modelmountpath=$modelroot
+
+
+	# local path and container mount path : modelmountpoint modellocalpoint
+	export modelmountpoint="$containermodelroot/$modelname/checkpoint_long"
+	export modellocalpoint="$modelmountpath/$modelname/checkpoint_long"
+
+	####@TODO MAke the 3 model file and checkpoint specific file
+	# $modelmountpath  = /a/model/models/$modelname/checkpoint_long
+	# $modellocalpoint # The root where we will mount the files
+	# Files :
+	# .ckpt-195000.index .ckpt-195000.meta .data-00000-of-00001 model_gia-ds-fpolsonwill_v02_210424_new_195000. checkpoint
+	# 3 files
+	checkpointpad=$checkpointno'000'
+	mindex='.ckpt-'$checkpointpad'.index'
+	mmeta='.ckpt-'$checkpointpad'.meta'
+	#.ckpt-195000.data-00000-of-00001
+	mdata='.ckpt-'$checkpointpad'.data-00000-of-00001'
+	mfilesuffix='.ckpt-'$checkpointpad
+	mfilepresuffix='_'$checkpointpad
+
+	# Content of the ./checkpoint file
+	#>>model_checkpoint_path: "model_gia-ds-fpolsonwill_v02_210424_new_195000.ckpt-195000"
+	#>>all_model_checkpoint_paths: "model_gia-ds-fpolsonwill_v02_210424_new_195000.ckpt-195000"
+	echo "Working in  $modellocalpoint"
+	tmpmodelfilename=$(cd $modellocalpoint;ls *$mmeta)
+	replstring='_'$checkpointpad$mmeta
+	secondString=""
+	tmpbasename="${tmpmodelfilename/$replstring/$secondString}"
+
+	checkpointbasefilename=$tmpbasename
+	mcheckpointfilecontentline1='model_checkpoint_path: "'$checkpointbasefilename$mfilepresuffix$mfilesuffix'"'
+	mcheckpointfilecontentline2='all_model_checkpoint_paths: "'$checkpointbasefilename$mfilepresuffix$mfilesuffix'"'
+	astia_server_file_location='/tmp/astiatmp'
+	mkdir -p $astia_server_file_location
+
+	mcheckpointfilepath=$astia_server_file_location'/'$modelname'_checkpoint_'$checkpointno
+	mindexfile=$checkpointbasefilename$mfilepresuffix$mindex
+	mmetafile=$checkpointbasefilename$mfilepresuffix$mmeta
+	mdatafile=$checkpointbasefilename$mfilepresuffix$mdata
+
+	testing=0
+	if [ "$testing" == "1" ]; then
+
+	echo "-----------------------------------------------Tests: "
+	#echo "- tmpmodelfilename=$tmpmodelfilename"
+	#echo "- replstring=$replstring"
+	#echo "- checkpointbasefilename=$checkpointbasefilename"
+	#echo "- mcheckpointfilecontent=$mcheckpointfilecontent"
+	#echo "- remove : $replstring in $tmpmodelfilename and use to construct the checkpoint file"
+	#echo "-- it gives: $tmpbasename"
+	echo "-- So the checkpoint file content is >>"
+	echo "$mcheckpointfilecontentline1"
+	echo "$mcheckpointfilecontentline2"
+	echo "<<"
+	#echo "- checkpointbasefilename=$checkpointbasefilename"
+	#echo "- mfilepresuffix=$mfilepresuffix"
+	#echo "- mfilesuffix=$mfilesuffix"
+	echo "- mindexfile=$mindexfile"
+	echo "- mmetafile=$mmetafile"
+	echo "- mdatafile=$mdatafile"
+	fi
+	# Making the local checkpoint we will mount
+	filecontent="$mcheckpointfilecontentline1"$'\n'"$mcheckpointfilecontentline2"
+	echo "$mcheckpointfilecontentline1" > $mcheckpointfilepath
+	echo "$mcheckpointfilecontentline2" >> $mcheckpointfilepath
+	# Making path to mount from local to the docker host foreach of the 3 files
+	##modelmountpoint modellocalpoint
+	export modelmountpointmeta=$modelmountpoint/$mmetafile
+	export modelmountpointindex=$modelmountpoint/$mindexfile
+	export modelmountpointdata=$modelmountpoint/$mdatafile
+	export modelmountpointckfile=$modelmountpoint/checkpoint
+	export modellocalpointmeta=$modellocalpoint/$mmetafile
+	export modellocalpointindex=$modellocalpoint/$mindexfile
+	export modellocalpointdata=$modellocalpoint/$mdatafile
+
+	echo "------------------------------------"
+	echo "-- File created :  cat $mcheckpointfilepath"
+	export MCHECKPOINTFILEPATH=$mcheckpointfilepath
+	export MCHECKPOINTCONTENT=$filecontent
+	echo "------------ or use : \$MCHECKPOINTFILEPATH"
+	echo "------------ or use : \$MCHECKPOINTCONTENT"
+}
