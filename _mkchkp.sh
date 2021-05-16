345e11c10dcac5d657d6fe19b9a12d20075884a8
JGWill-Orko <jgi@jgwill.com>
2021-05-16 06:48:49 -0400

    style

--- _mkchkp.sh
+++ _mkchkp.sh
@@ -6,14 +6,19 @@
 if [ -e $binroot/__fn.sh ]; then
                 source $binroot/__fn.sh $@
 fi
-if [ ! -d "checkpoint_long" ] || [ "$1" == "" ] ; then #must be in a model folder to run this
-	msg_not_ok "Mush run in a root folder of a model."
+if [ ! -d "checkpoint_long" ] ; then #must be in a model folder to run this
+	msg_error "Mush run in a root folder of a model."
+  echo "Usage $0 <checkpoint number>"
+	exit 2
+fi
+if [ "$1" == "" ] ; then #must be in a model folder to run this
+	msg_error "Argument missing"
   echo "Usage $0 <checkpoint number>"
 	exit 2
 fi
 re='^[0-9]+$'
 if ! [[ $1 =~ $re ]] ; then
-   msg_not_ok "error: Argument Not a number (checkpoints are numbers)" >&2; exit 1
+   msg_critical "error: Argument Not a number (checkpoints are numbers)" >&2; exit 1
 fi
 
 ## tODO : run from tpour model root folder (where ./checkpoint_long is seen in the ls)
@@ -36,13 +41,12 @@ cp ./checkpoint_long/*$1* $d/checkpoint_long && echo "Checkpoints file $1ik were
 sleep 1
 cp ./checkpoint_long/checkpoint $d/checkpoint_long
 
-echo "Editing your new checkpoint file...:"
+d "Editing your new checkpoint file...:"
 
 makecheckpointfile $modelname $1
 
 cp -f $MCHECKPOINTFILEPATH $d/checkpoint_long/checkpoint && msg_success "Created checkpoint file" || msg_failed "Creating checkpoint file"
 
-echo "Comitting model..."
 
 if [ "$2" == "-m" ]; then #Commit now an option 
 	echo "Comitting model..."
@@ -51,5 +55,5 @@ else
 	msg_info "Model was not comitted. (use -m next time)"
 fi
 cd $cdir
-msg_info "We are in this new model directory now"
+msg_info "$d created."
 
