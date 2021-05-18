24c9f1e2662bd6927fc0f392c57f709ae29f4737
JGWill-Orko <jgi@jgwill.com>
2021-05-18 07:07:54 -0400

    add(sample):newline,semicolon-split-array

--- sample-various.sh
+++ sample-various.sh
@@ -7,3 +7,9 @@ if ! [[ $1 =~ $re ]] ; then
    msg_critical "error: Argument Not a number (checkpoints are numbers)" >&2; exit 1
 fi
 
+
+#@STCGoal Split on space,semi-colon,newline to create an array
+y=(${TORENDER//$';'/ }) #semicolor
+y=(${x//$'\n'/ }) #newline
+y=(${x//$' '/ }) #space
+
