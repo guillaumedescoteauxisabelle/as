ccf893175349cdd668c0e76eb70e22209ae1561a
Jean Guillaume <jgi@jgwill.com>
2021-05-18 07:24:11 -0400

    chg:Enhanced fix input + conf

--- _syncFromAS.sh
+++ _syncFromAS.sh
@@ -1,13 +1,23 @@
-
-source _env.sh
-source _syncconf.sh $1
-if [ "$1" == " " ]; then # Need an arg
-	echo "usage:  $0 <modelname>"
-	exit 1
+if [ -e _env.sh ]; then #local or binroot
+        source _env.sh
+else
+        source $binroot/_env.sh
 fi
 
-echo "executing"
-echo rsync -aP "jgi@as:/a/model/models/$modelname/checkpoint_long" "/mnt/a/model/models/$modelname"
+if [ -e _syncconf.sh ]; then #source local
+        source _syncconf.sh $1
+else
+        source $binroot/_syncconf.sh $1
+fi
+
+if [ "$1" == " " ]; then # Need an argi
+        echo "usage:  $0 <modelname>"
+        exit 1
+fi
+#rsync -aP $localmodelpath jgi@orko:$remotemodelpath/
+echo "Sycing xecuting"
+#echo rsync -aP "jgi@as:/a/model/models/$modelname/checkpoint_long" "/mnt/a/model/models/$modelname"
+echo rsync -aP "jgi@as:/$remotemodelpath/checkpoint_long" "$localmodelpath"
 sleep 1
-rsync -aP "jgi@as:/a/model/models/$modelname/checkpoint_long" "/mnt/a/model/models/$modelname"
+rsync -aP "jgi@as:/$remotemodelpath/checkpoint_long" "$localmodelpath"
 
--- _syncToOrko.sh
+++ _syncToOrko.sh
@@ -1,7 +1,20 @@
-source _env.sh
-source _syncconf.sh $1
+#!/bin/bash
+#Sync from Orko
+#$localmodelpath $remotemodelpath
+if [ -e _env.sh ]; then #local or binroot
+	source _env.sh
+else
+	source $binroot/_env.sh
+fi
+
+if [ -e _syncconf.sh ]; then #source local
+	source _syncconf.sh $1
+else
+	source $binroot/_syncconf.sh $1
+fi
+
 if [ "$1" == " " ]; then # Need an argi
 	echo "usage:  $0 <modelname>"
 	exit 1
 fi
-rsync -aP $modelname jgi@orko:/a/model/models/
+rsync -aP $localmodelpath jgi@orko:$remotemodelpath/
