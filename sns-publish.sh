d8506565ef32064db6c64a627c3aea3de50ef344
JGWill-Orko <jgi@jgwill.com>
2021-05-22 04:33:10 -0400

    chg:autocomplete

--- sns-publish.sh
+++ sns-publish.sh
@@ -35,3 +35,46 @@ LASTREQUIREDARG=$2
 #LASTREQUIREDARG=NONE
 
 
+
+#########################################
+#Displays the application usage and startup info
+startapp "Notification CLI - AWS SNS Publish" \
+	 "Guillaume Descoteaux-Isabelle" \
+	  2021 \
+	  "
+Usage $0 <Subject> <Message> [topicARNl]
+        MORE" \
+	$LASTREQUIREDARG
+#@TODO set usage  ABOVE
+################################
+
+
+
+dowork "Sending Messages"
+
+#Here is what it does codified
+#@TODO BE CREATIVE ABOVE, ALL THE PREP IS DONE ;)
+tarn=$astiaarn
+
+if [ "$tarn" == "" ]; then echo "usage: $0 <subject> <message> [topicARN]";echo "Must specify a topic arn :  astiaarn variable is empty.  export astiaarn=myarn" ; exit 1;fi
+
+read -r -d '' MSGCONTENT << MSG
+##############################################################
+#
+#  $2
+#
+##############################################################
+#
+#
+#
+MSG
+
+aws sns publish --topic-arn  $tarn --subject "$1" --message "$MSGCONTENT" &> /dev/null && echo "Message sent" || (echo "Failed to send" && exit 1)
+
+
+##############END CODING HERE and define EXIT CODE somehow
+EXIT_CODE=0 #Define exit code
+MSG_WHEN_DONE=""
+########################################
+donework "$MSG_WHEN_DONE" $EXIT_CODE 
+
