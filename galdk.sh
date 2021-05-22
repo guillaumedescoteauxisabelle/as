45929d3c8b36899c5f162ed1c5626dbfec8df738
JGWill-Orko <jgi@jgwill.com>
2021-05-22 05:28:27 -0400

    fix:autocomplete

--- galdk.sh
+++ galdk.sh
@@ -13,25 +13,17 @@ fi
 
 ################AUTOCOMPLETION
 if [ "$1" == "--get-completions" ]; then #echo completion
-        echo "Autocompletion_UNDEFINED"
-        echo 'see:$binroot/etc/autocompletion/galdk.autocomplete'
-        echo "FILL_OUT_THIS_Section_in:$0:--get-completions"
+	#ls -d */
         #getmodellist
         #getmodelcheckpoints $2
         exit 0
 fi
 
-###########DEbug
 DEBUG=0
-d "Debug is Active"
-
-#Loads env if one in current dir (_env.sh)
-envif $@
-
 
 #@TODO Set the last ARG to the one required so it will exit if its not there
 ## Set to: NONE 	# if no args
-LASTREQUIREDARG=$1
+LASTREQUIREDARG=$2
 #LASTREQUIREDARG=NONE
 
 #Looks if we used a quiet mode :  
@@ -39,27 +31,67 @@ lookquiet $@
 
 #########################################
 #Displays the application usage and startup info
-startapp "XYZ Utilities - " \
+startapp "GIA Gallery Maker - Create using Docker" \
 	 "Guillaume Descoteaux-Isabelle" \
 	  2021 \
 	  "
-Usage $0 <arg1> <arg2> [argOptionel]
-        MORE" \
+Usage $0 <INPUTDIR> <OUTPUTDIR> [title] [footer] [toplevelsitename] 
+        " \
 	$LASTREQUIREDARG
 #@TODO set usage  ABOVE
 ################################
 
 
 
-dowork "MSG_WHEN_WE_GO"
+dowork "We are creating the gallery"
 
 #Here is what it does codified
 #@TODO BE CREATIVE ABOVE, ALL THE PREP IS DONE ;)
+containertag=guillaumeai/server:gal
+runscript=/a/bin/gallery_html_maker2.sh
+indir=$1
+if [ "$1" == "." ]; then # we use current dir path
+        indir=$(pwd)
+fi
 
+#inbase=$(basename $indir)
+
+outdir=$2
+if [ "$2" == "--suffix" ] || [ "$2" == "--s" ] || [ "$2" == "--gal" ]; then # We use same dir with suffix
+        sufix='__gal'
+        if [ "$3" != "" ]; then # we have a suffix supplied
+                sufix=$3 ; fi
+
+		outdir=$(cd $indir ; pwd)$sufix
+		mkdir -p $outdir
+		echo "Outdir made : $outdir"
+
+
+fi
+
+if [ -d "$indir" ] ; then
+	
+	#get fullpath of both in and out dir
+	indir=$(cd $indir;pwd)
+        mkdir -p $outdir
+        outdir=$(cd $outdir;pwd)
+	reldir=$(echo "$outdir" | sed -e 's/\/a\/lib\/results\///g')
+	wwwurl="http://as.guillaumeisabelle.com/$reldir"
+        #outbase=$(basename $outdir)
+
+	#docker run -it --rm -v $(pwd $outdir):/output $(pwd $indir):/input $(pwd):/work $containertag $runscript /input /output
+	echo docker run -it --rm -v $(pwd):/work  -v $outdir:/output -v $indir:/input  $containertag $runscript /input /output  "$3" "$4" "$5" 
+	sleep 1
+	docker run -it --rm -v $(pwd):/work  -v $outdir:/output -v $binroot:/a/bin -v $HOME:/home/jgi -v $indir:/input  $containertag $runscript /input /output "$3" "$4" "$5" && \
+		echo "$wwwurl"
+else
+	donework "Input dir not existent" -1
+	exit -1
+fi
 
 ##############END CODING HERE and define EXIT CODE somehow
 EXIT_CODE=0 #Define exit code
-MSG_WHEN_DONE=""
+MSG_WHEN_DONE="We are done fine"
 ########################################
 donework "$MSG_WHEN_DONE" $EXIT_CODE 
 
