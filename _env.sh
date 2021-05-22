510d21507fcb8a8c69f45f7108bda155e1fc32db
JGWill-Orko <jgi@jgwill.com>
2021-05-21 23:44:29 -0400

    orko-crap

--- _env.sh
+++ _env.sh
@@ -5,12 +5,12 @@
 BINROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # used to update the DNS
-export asdnstoken="MYTOKEN"
+
 export GIADEFAULTFILENAMESEPARATOR="__"
 
 
 
-dnstoken="2ac6e31108282aaa24de43d4f45213ef76e566f563bf5649eac74c9be07dde3f"
+#dnstoken="2ac6e31108282aaa24de43d4f45213ef76e566f563bf5649eac74c9be07dde3f"
 
 
 ############################ENDING###############
@@ -22,7 +22,7 @@ echo $hostbinenvfile
 if [ -e $hostbinenvfile ]; then
     . $hostbinenvfile
 else
-    msg "vi $hostbinenvfile  - does not exist, create it if you require to define specific to platform variable or overwrite some." 
+     echo "vi $hostbinenvfile  - does not exist, create it if you require to define specific to platform variable or overwrite some." 
     #&> /dev/null 
 
 fi
