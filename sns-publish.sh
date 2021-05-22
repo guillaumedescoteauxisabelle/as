8a08a03b608c1c22014f263083b95574a234f5d5
JGWill-Orko <jgi@jgwill.com>
2021-05-22 04:32:02 -0400

    chg:

--- sns-publish.sh
+++ sns-publish.sh
@@ -8,7 +8,18 @@ if [ -e $binroot/__fn.sh ]; then
 	        source $binroot/__fn.sh $@
 fi
 
-#__REPLACEMENTHEAD__
+#sns-publish
+#sns-publish.sh
+
+################AUTOCOMPLETION
+if [ "$3" == "--get-completions" ]; then #echo completion
+        echo "@TODO List topic ARN"
+      
+       
+        #getmodellist
+        #getmodelcheckpoints $2
+        exit 0
+fi
 
 ###########DEbug
 DEBUG=0
@@ -23,48 +34,4 @@ d "Debug is Active"
 LASTREQUIREDARG=$2
 #LASTREQUIREDARG=NONE
 
-#Looks if we used a quiet mode :  
-#lookquiet $@
-
-#########################################
-#Displays the application usage and startup info
-startapp "Notification CLI - AWS SNS Publish" \
-	 "Guillaume Descoteaux-Isabelle" \
-	  2021 \
-	  "
-Usage $0 <Subject> <Message> [topicARNl]
-        MORE" \
-	$LASTREQUIREDARG
-#@TODO set usage  ABOVE
-################################
-
-
-
-dowork "Sending Messages"
-
-#Here is what it does codified
-#@TODO BE CREATIVE ABOVE, ALL THE PREP IS DONE ;)
-tarn=$astiaarn
-
-if [ "$tarn" == "" ]; then echo "usage: $0 <subject> <message> [topicARN]";echo "Must specify a topic arn :  astiaarn variable is empty.  export astiaarn=myarn" ; exit 1;fi
-
-read -r -d '' MSGCONTENT << MSG
-##############################################################
-#
-#  $2
-#
-##############################################################
-#
-#
-#
-MSG
-
-aws sns publish --topic-arn  $tarn --subject "$1" --message "$MSGCONTENT" &> /dev/null && echo "Message sent" || (echo "Failed to send" && exit 1)
-
-
-##############END CODING HERE and define EXIT CODE somehow
-EXIT_CODE=0 #Define exit code
-MSG_WHEN_DONE=""
-########################################
-donework "$MSG_WHEN_DONE" $EXIT_CODE 
 
