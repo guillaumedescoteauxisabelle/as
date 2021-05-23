4f46dd54928234a99d1b72fb78bdde24fbaaf5c7
JGWill-Orko <jgi@jgwill.com>
2021-05-22 23:34:01 -0400

    import

---
+++ _chks_slit_dir.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Split into dir all CHKS
+export basedir=chks
+mkdir -p $basedir
+odir=$(pwd)
+for c in $chks; do tdir=$basedir/$c;mkdir -p $tdir ; cd $tdir ; cp $odir/*_$c'k.jpg' .; cd $odir; done
+
