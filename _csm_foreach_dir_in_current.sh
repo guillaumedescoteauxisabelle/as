e3a715d7ebcab1b0022571e49894c72256926ff8
JGWill-Orko <jgi@jgwill.com>
2021-05-22 23:14:06 -0400

    fix

--- _csm_foreach_dir_in_current.sh
+++ _csm_foreach_dir_in_current.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+cdir=$(pwd)
+
 for d in * 
 do 
-	cd $d
-	gis-csm ../_$d.csm.jpg & 
-	cd ..
+	if [ -d "$d" ] ; then
+		echo -n "$d..."
+		cd $d
+		gis-csm -d  &> /dev/null || echo "Failed to start gis-csm" & 
+		#sleep 1
+		cd $cdir
+		echo "...in progress"
+	fi
 done
