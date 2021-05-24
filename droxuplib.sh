0308027e63883bd757e4c01b6a3ac9e0c4f880c1
JGWill-Orko <jgi@jgwill.com>
2021-05-24 11:38:49 -0400

    chg:ADded validation of location

--- droxuplib.sh
+++ droxuplib.sh
@@ -7,9 +7,16 @@ fi
 
 
 #upload to droxul from lib without specifying as it reproduce the same path in Dropbox
+tst=$(pwd | cut -c1-6)
 
 # sub the path for the dropbox path
 td=$(pwd | sed -e 's/\/a\/lib/\/Lib/' | sed -e 's/\/c\/Users\/jeang\/Dropbox\/lib/\/Lib/') #/c/Users/jeang/Dropbox/lib
+tst=$(echo "$td" | cut -c2-4)
+if [ "$tst" != "Lib" ]; then #@State we were not in the right place
+	echo "Must be in a subdir of \$libroot.  ex.  /a/lib/results"
+	exit 1
+fi
+
 droxul mkdir $td   &> /dev/null # make the dir if it does not exist and dont tell us anything if that does not work
 
 # Upload exerything
