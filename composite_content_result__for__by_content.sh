e8ae51552b6cc94a01988532b61d3b585e157278
JGWill Gaia.WSL <jgi@jgwill.com>
2021-05-26 12:22:32 -0400

    add/chg: What is required for Labeling and montage of Content + Results

---
+++ composite_content_result__for__by_content.sh
@@ -0,0 +1,58 @@
+
+#@example 
+
+# Run this in the directory where you have your by_content files created by _result_by_content.sh
+#@Prerequisite _result_by_content.sh
+#@TODO _result_by_content.sh will require Upgrade
+
+
+for d in * ; do echo $d; cdir=$(pwd);cd $d; $binroot/composite_content_result.sh * ../_montage  ;cd $cdir ; done
+
+
+
+
+
+
+
+
+
+
+
+# REF
+exit
+
+
+f="$1"
+f2="$2"
+tdir="$3"
+mkdir -p $tdir
+
+
+tdir=$(cd $tdir;pwd) || exit 1
+mkdir -p "$tdir/L"
+
+pointsize=42
+tfont=Helvetica
+bgcolor='#000000'
+fillcolor='gray'
+
+fn=${f%.*}
+fn2=${f2%.*}
+fl=$(echo "$fn" | sed -e 's/___/   /g' |sed -e 's/__/  /g'|sed -e 's/_/ /g');
+fl2=$(echo "$fn2" | sed -e 's/___/   /g' |sed -e 's/__/  /g'|sed -e 's/_/ /g');
+out=$tdir/L/$fn'__L.jpg'
+out2=$tdir/L/$fn2'__L.jpg'
+outm=$tdir/$fn'__M.jpg'
+rx=$($binroot/imgGetResolution.sh $f x)
+ry=$($binroot/imgGetResolution.sh $f y)
+ry=$(expr $ry + $pointsize)
+rval=$rx'x'$ry
+
+#DEBUG=1
+#$dvar f fn fl r out tdir
+
+montage -label "$fl" -font $tfont -pointsize $pointsize -geometry +0+$pointsize -background "$bgcolor" -fill "$fillcolor" $f $out; 
+montage -label "$fl2" -font $tfont -pointsize $pointsize -geometry +0+$pointsize -background "$bgcolor" -fill "$fillcolor" $f2 $out2; 
+montage $out $out2  -geometry +100+0  -background "$bgcolor" -fill "$fillcolor" $outm
+
+#feh -F $out
---
+++ montage_label.sh
@@ -0,0 +1,27 @@
+
+
+f="$1"
+tdir="$2"
+mkdir -p $tdir
+tdir=$(cd $tdir;pwd) || exit 1
+
+pointsize=42
+tfont=Helvetica
+bgcolor='#000000'
+fillcolor='gray'
+
+fn=${f%.*}
+fl=$(echo "$fn" | sed -e 's/___/   /g' |sed -e 's/__/  /g'|sed -e 's/_/ /g');
+out=$tdir/$fn'__L.jpg'
+rx=$($binroot/imgGetResolution.sh $f x)
+ry=$($binroot/imgGetResolution.sh $f y)
+ry=$(expr $ry + $pointsize)
+rval=$rx'x'$ry
+
+DEBUG=1
+dvar f fn fl r out tdir
+
+montage -verbose -label "$fl" -font $tfont -pointsize $pointsize -geometry $rval -background "$bgcolor" -fill "$fillcolor" $f $out; 
+
+
+#feh -F $out
