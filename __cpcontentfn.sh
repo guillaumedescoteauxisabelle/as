888c5181a24ef8039b00343beff37c1d30eec6ea
JGWill Gaia.WSL <jgi@jgwill.com>
2021-05-26 11:42:32 -0400

    fix

--- __cpcontentfn.sh
+++ __cpcontentfn.sh
@@ -20,26 +20,32 @@ loopcpcontent() {
 cpcontent() {
 	c=$1
 	tdir=$2
+	cdir=$(pwd)
+	mkdir -p $tdir
+	tdir=$(cd $tdir;pwd)
+
 	cd $libroot/samples
 	for d in $(ls -d *) ; do
-		 if [ ! -d "$d" ] ; then 
-			 cd $d
-			 cp $c.jpg $tdir &> /dev/null || \
+		 if [ -d "$d" ] ; then 
+			 cd $d && \
+			 (cp $c.jpg $tdir &> /dev/null || \
 			 cp $c.png $tdir &> /dev/null || \
-			 cp $c $tdir &> /dev/null 
+			 cp $c $tdir &> /dev/null )
 		fi
 		 cd $libroot/samples
 	done
 	cd $libroot/datasets 
 	for d in $(ls -d *) ; do 
-		if [ ! -d "$d" ] ; then 
-			cd $d 
-			cp $c.jpg $tdir &> /dev/null  || \
+		if [ -d "$d" ] ; then 
+			cd $d && \
+			(cp $c.jpg $tdir &> /dev/null  || \
 			cp $c.png $tdir &> /dev/null  || \
-			cp $c $tdir &> /dev/null 
+			cp $c $tdir &> /dev/null )
 		fi 
 		cd $libroot/datasets
 	done
+
+	cd $cdir
 	}
 
 
