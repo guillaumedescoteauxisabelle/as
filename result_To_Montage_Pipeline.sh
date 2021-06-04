3078f8ec5b159035518a684cba280848edfcc13e
JGWill-Orko <jgi@jgwill.com>
2021-06-04 05:37:49 -0400

    stuff

--- result_To_Montage_Pipeline.sh
+++ result_To_Montage_Pipeline.sh
@@ -1,5 +1,6 @@
 $binroot/_result_by_content.sh 
 cd $(pwd)'_by_content' && \
+	echo "now in $(pwd)" && sleep 2 && \
 $binroot/composite_content_result__for__by_content.sh \
 || echo "Could not get in "$(pwd)'_by_content'
 
