079b801d9f1742ac12f8de54141b66e28a8d8cfd
JGWill-Orko <jgi@jgwill.com>
2021-06-04 06:09:01 -0400

    ouin

--- _result_by_content.sh
+++ _result_by_content.sh
@@ -5,10 +5,11 @@
 
 cdir=$(pwd)
 ftag=$(basename $cdir)
-echo "$cdir"
-sleep 1
-tdir=$cdir'_by_content'
+echo "cdir=$cdir"
 
+tdir=$cdir'_by_content'
+echo "tdir=$tdir"
+sleep 1
 
 #@STCIssue A new file with function was created
 . $binroot/__cpcontentfn.sh
@@ -33,62 +34,79 @@ tdir=$cdir'_by_content'
  #       done
 
 #}
-#refreshing the local src liste
-$binroot/refresh_source_list.sh
-$binroot/mk_source_list.sh &
+if [ "$1" != "--no-refresh" ] ; then
+	echo "refreshing the local src liste"
+	$binroot/refresh_source_list.sh
+	echo "refreshing the rendered files in here $(pwd)"
+	$binroot/mk_source_list.sh 
+else
+	echo "--no-refresh switch used, skipped refreshing source list"
+fi
+echo "cdir=$cdir"
+sleep 3
+cd $cdir
+echo "now in $(pwd)"
 
 for d in $(ls -d *x); do 
-	echo "Processing $d..."
-	
+	echo $d
+done
+echo "------------above is what will be processing "
+sleep 3
+criss=$(pwd)
+for d in $(ls -d *x); do 
+	echo "Processing $d..from cdir=$cdir."
+	cd $cdir
+	cd $criss
 #	if [ -d "$d" ] ;then 
-		
-		cd "$d"
-		for c in $(cat $libroot/samples/_srcfiles.txt); do 
-			tcdir="$tdir/$c" 
+	pwd	
+	cd $d
+	pwd
 
-			if [ ! -d "$tcdir" ] ; then
-				mkdir -p "$tcdir"
-				echo -n "."
-			fi
+	for c in $(cat $libroot/samples/_srcfiles.txt); do 
+		tcdir="$tdir/$c" 
+		if [ ! -d "$tcdir" ] ; then
+			mkdir -p "$tcdir"
+			echo -n "."
+		fi
 			
-			#@a Copy our content with the ftag pattern into the target names
-			#@STCGoal If any, we will want the original content available in ../
-			cp "$c"_"$ftag"*jpg "$tcdir" &> /dev/null  && \
-				cpcontent $c $tcdir/.. && \
-                                 echo -n "."
+		#@a Copy our content with the ftag pattern into the target names
+		#@STCGoal If any, we will want the original content available in ../
+		cp "$c"_"$ftag"*jpg "$tcdir" &> /dev/null  && \
+			cpcontent $c $tcdir/.. && \
+                        echo -n "."
 
-		done 
+	done 
 
-                for c in $(cat $libroot/datasets/_srcfiles.txt); do
+        for c in $(cat $libroot/datasets/_srcfiles.txt); do
 
-			#make a dir named as the content without ext
-                        tcdir="$tdir/$c"
-                        if [ ! -d "$tcdir" ] ; then
-                                mkdir -p "$tcdir"
-                       		echo -n "."
-                        fi
+		#make a dir named as the content without ext
+                tcdir="$tdir/$c"
+                if [ ! -d "$tcdir" ] ; then
+                        mkdir -p "$tcdir"
+                 	echo -n "."
+                 fi
 
-			#copy out file pattern matching out ftag and content name
-                        cp "$c"_"$ftag"*jpg "$tcdir" &> /dev/null && \
-				cpcontent $c $tcdir/.. && \
-				 echo -n "."
-                done
+		#copy out file pattern matching out ftag and content name
+                cp "$c"_"$ftag"*jpg "$tcdir" &> /dev/null && \
+			cpcontent $c $tcdir/.. && \
+			 echo -n "."        
+	done
 		
 
 
-                for c in $(cat $cdir/_srcfiles.txt); do
+        # for c in $(cat $cdir/_srcfiles.txt); do
 
-                        #make a dir named as the content without ext
-                        tcdir="$tdir/$c"
-                        if [ ! -d "$tcdir" ] ; then
-                                mkdir -p "$tcdir"
-                                echo -n "."
-                        fi
+         #     #make a dir named as the content without ext
+         #     tcdir="$tdir/$c"
+         #     if [ ! -d "$tcdir" ] ; then
+         #            mkdir -p "$tcdir"
+         #            echo -n "."
+         #     fi
 
-                        #copy out file pattern matching out ftag and content name
-                        cp "$c"_"$ftag"*jpg "$tcdir" &> /dev/null && \
-                                 echo -n "."
-                done
+         #        #copy out file pattern matching out ftag and content name
+         #         cp "$c"_"$ftag"*jpg "$tcdir" &> /dev/null && \
+         #                        echo -n "."
+         # done
 
 
 
@@ -96,8 +114,11 @@ for d in $(ls -d *x); do
 #	else 
 #		echo "skipping $d, not a directory ;)"
 #	fi
+echo -n "from $(pwd)..."
 	cd $cdir
-	echo "...done $d"
+	cd $criss
+	echo "to: $(pwd)"
+	echo "...done"
 done
 
 #Cleanup directory we wont need
