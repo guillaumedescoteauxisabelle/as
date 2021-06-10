db543acc333214bb2d85760f55b8c3adfb3dce65
JGWill-Orko <jgi@jgwill.com>
2021-06-10 11:45:14 -0400

    --label

---
+++ _csm_foreach_dir_in_current_As_Label.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cdir=$(pwd)
+
+for d in * 
+do 
+	if [ -d "$d" ] ; then
+		echo -n "$d..."
+		cd $d
+		gis-csm -d --label &> /dev/null || echo "Failed to start gis-csm" & 
+		sleep 1
+		cd $cdir
+		echo "...in progress"
+	fi
+done
