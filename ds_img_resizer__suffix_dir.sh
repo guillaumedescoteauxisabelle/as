08fb5ae7dfb703544d806d7a5efc7b3f9b5b1cb5
JGWill-Orko <jgi@jgwill.com>
2021-06-11 17:10:44 -0400

    enhanced:will resize if one of the two resolution is above desired

--- ds_img_resizer__suffix_dir.sh
+++ ds_img_resizer__suffix_dir.sh
@@ -2,7 +2,8 @@
 
 
 # Resize in a subdir created with above res suffix
-tsize=864x
+resolution=864
+tsize=$resolution'x'
 if [ "$1" != "" ]; then # We have specified a different res 
 	tsize=$(echo "$1" | sed -e 's/x//')'x'
 fi
@@ -15,14 +16,30 @@ tqual=100
 tres=$tsize
 tdir="$(pwd)-$tres"
 mkdir -p $tdir
+dsresizer() {
+	local tqual=$1;local res=$2;local f=$3;local targetfilepath=$4
+	if [ -e "$f" ] ; then 
+		convert -quality $tqual -resize $res $f $targetfilepath && echo -n "." || echo "Error with $f"
+	fi
+
+
+}
 for f in *.jpg *.png ; do 
 	if [ "$f" != '*.jpg' ] &&  [ "$f" != '*.png' ] ; then 
 	
 		res=$($binroot/imgGetResolution.sh $f m $tres)
-	
+		#@status, those bellow desired res wont be resized
+		rx=$($binroot/imgGetResolution.sh $f x)
+		ry=$($binroot/imgGetResolution.sh $f y)
+
+		targetfilepath=$tdir/$ff.jpg
 		ff=${f%.*}
-	
-		convert -quality $tqual -resize $res $f $tdir/$ff.jpg && echo -n "." || echo "Error with $f"
+
+		#@a If one of the two resolution are higher that the desired resolutio, we resize
+		if [ $rx -gt $resolution ]  ||  [ $ry -gt $resolution ]; then 
+			#convert -quality $tqual -resize $res $f $targetfilepath && echo -n "." || echo "Error with $f"
+			dsresizer $tqual $res "$f" "$targetfilepath" 
+		fi
 	fi
 done
 echo "done.  cd $tdir"
