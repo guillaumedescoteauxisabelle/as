370a436d60458a4b132811c3ec08e684569a0129
JGWill-Orko <jgi@jgwill.com>
2021-06-16 22:26:13 -0400

    add:tst-proxy

---
+++ sample-montage.gal-dbg_v01b.sh
@@ -0,0 +1,17 @@
+#sdir=_loved_series_by_DavidNPierreT_210602\ntdir=$sdir'__gal'\n$binroot/gallery_html_maker2.sh $sdir $sdir'__gal' "Loved DBG+PT 210602" "Content and Stylization was created by Guillaume D.Isabelle in my Master in Art at UQAC/Nad, 2021" Loved_DBGv01b__210602 && tar cf - $tdir | (cd /home/jgi/astiapreviz/\n tar xf -\n ./_getaddress.sh $tdir)
+
+sdir=_montage
+tdir=$sdir'__gal'
+$binroot/gallery_html_maker2.sh \
+	$sdir \
+	$tdir \
+	"Ups DBG 210602" \
+	"Content and Stylization was created by Guillaume D.Isabelle in my Master in Art at UQAC/Nad, 2021" \
+	"Ups_DBGv01b__210602" 
+
+#	&& tar cf - $tdir \
+#		| (\
+#		cd /home/jgi/astiapreviz/  ; \
+#		tar xf -; \
+#		./_getaddress.sh $tdir)
+
---
+++ sample-publish_gal__sample.sh
@@ -0,0 +1,1 @@
+tar cf - _montage-dbg_v01b2_by_content__315k__gal | (cd /home/jgi/astiapreviz/; tar xf -; ./_getaddress.sh _montage-dbg_v01b2_by_content__315k__gal --sns)
