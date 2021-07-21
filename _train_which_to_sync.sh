063ea24de9a562837f7774910b9c63881172b1a2
Jean Guillaume <jgi@jgwill.com>
2021-07-21 02:47:07 -0400

    working

--- _train_which_to_sync.sh
+++ _train_which_to_sync.sh
@@ -1,6 +1,20 @@
-m=model_gia-ds-DavidBouchardGagnon-v02-210702-864x_new
+m=$1
+if [ "$1" == "" ] ; then 
+	echo "Usage : $0 <modelname>"
+else
 
 rhn=jgi@orko #rendering host (where to sync)
 thn=jgi@as #training host
 
-rh=$(ssh $rhn "cd /a/bin;./mckinfo.sh $m");th=$(ssh $thn "cd /a/bin;./mckinfo.sh $m");for t in $rh; do th=$(sed -e 's/'"$t "'//g'<<< $th) ;done; echo "$th"
+rh=$(ssh $rhn "cd /a/bin;./mckinfo.sh $m")
+th=$(ssh $thn "cd /a/bin;./mckinfo.sh $m")
+
+echo $rh
+echo $th
+#echo "$rh" | sed -e 's/'"$th"'//g'
+for t in $rh; do 
+	th=$(sed -e 's/'"$t "'//g'<<< $th) 
+done; echo "$th"
+
+
+fi
