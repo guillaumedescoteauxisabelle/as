0cc7ac8238e4b0a4a43f0e7a8e805750f83e8199
Jean Guillaume <jgi@jgwill.com>
2021-07-21 03:01:33 -0400

    ok

--- _train_which_to_sync.sh
+++ _train_which_to_sync.sh
@@ -9,30 +9,30 @@ thn=jgi@as #training host
 rh=$(ssh $rhn "cd /a/bin;./mckinfo.sh $m")
 th=$(ssh $thn "cd /a/bin;./mckinfo.sh $m")
 
-echo $rh
-echo $th
+#echo $rh
+#echo $th
 #echo "$rh" | sed -e 's/'"$th"'//g'
 
 o=" "
 for t in $th; do 
-	echo -n "entering : $t..."
+	#echo -n "entering : $t..."
 	isdone="0"
 	for r in $rh ; do 
-		echo -n "$r, "
+		#echo -n "$r, "
 		if [ "$t" == "$r" ] ; then isdone="1" ; fi
 
 	done
 	if [ "$isdone" == "1" ] ; then 
 		#o+=" $t" 
-		echo "$t is done"
+		#echo "$t is done"
 	else
-		echo "$t isn't"
+		#echo "$t isn't"
 		o="$o $t"
 	fi
 	#th=$(sed -e 's/'"$t "'//g'<<< $th) 
 done; 
 #echo "$th"
-echo "-----------What needs to be sync -------------"
+#echo "-----------What needs to be sync -------------"
 echo "$o"
 
 fi
