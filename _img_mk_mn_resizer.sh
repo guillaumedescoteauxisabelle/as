45c3bcc49fb0cc496aa26a09bd012f77cb300acb
Jean Guillaume <jgi@jgwill.com>
2022-04-10 16:39:44 -0400

    refacto

---
+++ _img_mk_mn_resizer.sh
@@ -0,0 +1,31 @@
+mnresizer() {
+	local _tres=$1
+	local _tdir="$2"
+
+	local _tqual="$3"
+	if [ "$_tqual" == "" ]; then _tqual=100; fi
+	mkdir -p $tdir
+	for f in *jpg; do
+		 convert "$f" -resize $_tres'x' -quality $_tqual "$_tdir/$f"
+	done
+	for f in *png; do 
+		convert "$f" -resize $_tres'x' -quality $_tqual "$_tdir/$f"
+	done
+}
+
+# Normal
+tres=600
+tqual=100
+tdir=mn;mkdir -p $tdir
+for f in *jpg; do 
+	convert "$f" -resize $tres'x' -quality $tqual "$tdir/$f"
+done
+
+# tn
+mnresizer  tn 150 80
+
+# Center cropped
+tdir=mncc;mkdir -p $tdir
+for f in *jpg; do 
+	convert "$f" -resize $tres'x'$tres\! -quality $tqual "$tdir/$f"
+done
