3970c20d89220abe50615df9c1ccb446f7b9ce59
Jean Guillaume <jgi@jgwill.com>
2022-04-10 19:30:39 -0400

    chg:fn

--- __fn.sh
+++ __fn.sh
@@ -966,3 +966,21 @@ dst() {
 dstm() {
 	if [ "$MESSAGE" ];then echo "$MESSAGE";fi
 }
+
+
+
+mnresizer() {
+        local _tres="$2"
+        local _tdir=$1
+
+        local _tqual=100
+        if [ "$3" != "" ]; then _tqual=$3; fi
+				
+        mkdir -p $_tdir
+        for f in *jpg; do
+                 convert "$f" -resize $_tres'x' -quality $_tqual "$_tdir/$f"
+        done
+        for f in *png; do
+                convert "$f" -resize $_tres'x' -quality $_tqual "$_tdir/$f"
+        done
+}
