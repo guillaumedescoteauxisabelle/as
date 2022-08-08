535e0d3420593937f69daad2ba5ce29f47b54155
JGWill Gaia.WSL <jgi@jgwill.com>
2022-08-08 16:14:52 -0400

    add dkGetContainerNameByPort.sh

--- dkGetContainerNameByPort.sh
+++ dkGetContainerNameByPort.sh
@@ -6,8 +6,9 @@ cmds=$binroot/$slister
 #echo $cmds
 rcontainer=$($cmds | tr ":" " " | awk '/'$1'/ { print $2 }')
 
-if [ "$rcontainer" == "" ]; then
+if [ "$rcontainer" == "" ] ; then
 	# Get from Stopped list
+	echo "TODO get from  stopped and start it maybe"
 else # Print it out
 	echo $rcontainer
 fi
