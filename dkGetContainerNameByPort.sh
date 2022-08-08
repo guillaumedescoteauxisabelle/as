7517ecfef039e1f1536d13e7692c89622230abe0
JGWill Gaia.WSL <jgi@jgwill.com>
2022-08-08 16:13:59 -0400

    add dkGetContainerNameByPort.sh

--- dkGetContainerNameByPort.sh
+++ dkGetContainerNameByPort.sh
@@ -5,4 +5,9 @@ slister=dkcListPort.sh
 cmds=$binroot/$slister
 #echo $cmds
 rcontainer=$($cmds | tr ":" " " | awk '/'$1'/ { print $2 }')
-echo $rcontainer
+
+if [ "$rcontainer" == "" ]; then
+	# Get from Stopped list
+else # Print it out
+	echo $rcontainer
+fi
