44c8d4e17a94b9fc4a8b126ac002f1b094e92e2a
JGWill Gaia.WSL <jgi@jgwill.com>
2022-08-08 16:12:42 -0400

    add dkGetContainerNameByPort.sh

--- dkGetContainerNameByPort.sh
+++ dkGetContainerNameByPort.sh
@@ -4,5 +4,5 @@
 slister=dkcListPort.sh
 cmds=$binroot/$slister
 #echo $cmds
-$cmds | tr ":" " " | awk '/'$1'/ { print $2 }'
-
+rcontainer=$cmds | tr ":" " " | awk '/'$1'/ { print $2 }'
+echo $rcontainer
